d73fdb534a976a6b2149c895be6a86af4b738570
Aimuel Emmanuel <aimuelemmanuel@gmail.com>
2023-03-15 14:09:09 +0100

    first commit

--- src/components/HeroThree.js
+++ src/components/HeroThree.js
@@ -6,7 +6,7 @@ import confetti from "../images/image-confetti.jpg";
 const HeroThree = () => {
   return (
     <div className="bg-slate-50 lg:-translate-y-[20rem] pb-16">
-      <div className=" w-10/12 mx-auto pt-16">
+      <div className=" w-9/12 mx-auto pt-16">
         <p className="text-4xl text-slate-700 pb-8">Latest Articles</p>
         <div className="flex md:space-x-2 flex-col text-center md:text-left md:flex-row space-y-12 md:space-y-0 justify-between">
           <div className="flex flex-col space-y-1 bg-white pb-6 md:pb-2">
@@ -19,9 +19,11 @@ const HeroThree = () => {
               currency with no fees
             </span>
             <span className="text-slate-400 text-xs px-2">
-              The world is getting smaller and <br className="hidden md:block" />
+              The world is getting smaller and{" "}
+              <br className="hidden md:block" />
               we're becoming more mobile. So <br className="hidden md:block" />
-              why should you be forced to only <br className="hidden md:block" />
+              why should you be forced to only{" "}
+              <br className="hidden md:block" />
               receive money in a single...
             </span>
           </div>
@@ -37,7 +39,8 @@ const HeroThree = () => {
             <span className="text-slate-400 text-xs px-2">
               Our simple budgeting feature <br className="hidden md:block" />
               allows you to seperate out your <br className="hidden md:block" />
-              spending and set realistic limits <br className="hidden md:block" />
+              spending and set realistic limits{" "}
+              <br className="hidden md:block" />
               each month. That means you...
             </span>
           </div>
@@ -51,7 +54,8 @@ const HeroThree = () => {
               wherever you go
             </span>
             <span className="text-slate-400 text-xs px-2">
-              we want you to enjoy your travels. <br className="hidden md:block" />
+              we want you to enjoy your travels.{" "}
+              <br className="hidden md:block" />
               This is why we don't charge any <br className="hidden md:block" />
               fees on purchases while you're <br className="hidden md:block" />
               abroad. We'll even show you...
@@ -68,8 +72,10 @@ const HeroThree = () => {
             </span>
             <span className="text-slate-400 text-xs px-2">
               After a lot of hard work by the <br className="hidden md:block" />
-              whole team, we're excited to launch <br className="hidden md:block" />
-              our closed beta. It's easy to request <br className="hidden md:block" />
+              whole team, we're excited to launch{" "}
+              <br className="hidden md:block" />
+              our closed beta. It's easy to request{" "}
+              <br className="hidden md:block" />
               an invite throught the site...
             </span>
           </div>
